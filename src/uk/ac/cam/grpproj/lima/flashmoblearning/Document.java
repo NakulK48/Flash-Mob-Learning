75d21313fa8a1044c828d7f6b1d9087702364b2c
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-27 10:54:56 +0000

    Move to a package

---
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/Document.java
@@ -0,0 +1,5 @@
+package uk.ac.cam.grpproj.lima.flashmoblearning;
+
+public interface Document {
+
+}
--- src/uk/ac/cam/grpproj/lima/flashmoblearning/WIPDocument.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/WIPDocument.java
@@ -1,3 +1,4 @@
+package uk.ac.cam.grpproj.lima.flashmoblearning;
 
 public class WIPDocument implements Document {
 

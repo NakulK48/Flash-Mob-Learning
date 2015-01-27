03f7e225b8a4afd0d17a5908baedc749242db5e5
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-27 15:07:47 +0000

    Fork

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/PublishedDocument.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/PublishedDocument.java
@@ -7,4 +7,6 @@ public abstract class PublishedDocument extends Document{
 	/** Get the one and only revision */
 	public abstract Revision getContentRevision();
 
+	/** Copy a document so we can edit it */
+	public abstract WIPDocument fork(User newOwner);
 }

c10ecdcf4c55ea13a0ed15ca89011b7713828082
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-27 14:27:11 +0000

    Docs

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/Student.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/Student.java
@@ -1,5 +1,7 @@
 package uk.ac.cam.grpproj.lima.flashmoblearning;
 
+/** A Student, i.e. an ordinary user. Just a marker class for now, User implements everything
+ * important. */
 public abstract class Student extends User {
 
 }
--- src/uk/ac/cam/grpproj/lima/flashmoblearning/Teacher.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/Teacher.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.grpproj.lima.flashmoblearning;
 
+/** Administrative account. (Almost) everything is implemented in User, so this is a marker. */
 public abstract class Teacher extends User {
 
 }

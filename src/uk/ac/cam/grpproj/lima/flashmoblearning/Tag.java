1e86e571916cce6d8409be214e7ccbf7452ed9a4
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-27 15:36:16 +0000

    Flesh out Tag

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/Tag.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/Tag.java
@@ -1,5 +1,18 @@
 package uk.ac.cam.grpproj.lima.flashmoblearning;
 
-public class Tag {
+public abstract class Tag {
+	
+	/** Tag name */
+	public final String name;
+	
+	public Tag(String n) {
+		name = n;
+	}
+	
+	/** Has the tag been banned? */
+	public abstract boolean getBanned();
+	
+	/** Ban or unban the tag */
+	public abstract void setBanned(boolean banned);
 
 }

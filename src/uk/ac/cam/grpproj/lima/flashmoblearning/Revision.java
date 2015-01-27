7c90c7fc6e48ccc0a7d8575567a4cc6a7713f747
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-27 15:08:06 +0000

    Revision's are owned by somebody

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/Revision.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/Revision.java
@@ -2,8 +2,12 @@ package uk.ac.cam.grpproj.lima.flashmoblearning;
 
 import java.util.Date;
 
-/** Represents a single version of a document. */
+/** Represents a single version of a document. Note that this does not necessarily correspond to a
+ * single database table. */
 public abstract class Revision {
+	
+	/** Owned by a specific user */
+	public abstract User getOwner();
 	
 	/** Created at... */
 	public abstract Date getCreationTime();

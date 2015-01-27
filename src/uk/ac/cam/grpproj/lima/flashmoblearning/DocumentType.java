2555cb31db8fb0081d68ae52b90772bfdbc444a8
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-27 14:21:33 +0000

    Fix build for removing Editor

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/DocumentType.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/DocumentType.java
@@ -2,40 +2,14 @@ package uk.ac.cam.grpproj.lima.flashmoblearning;
 
 public enum DocumentType {
 	
-	PLAINTEXT(false) {
-
-		@Override
-		public Editor createEditor(Document d, boolean runOnly) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-	},
-	SKULPT(true) {
-
-		@Override
-		public Editor createEditor(Document d, boolean runOnly) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-	},
-	PI(true) {
-
-		@Override
-		public Editor createEditor(Document d, boolean runOnly) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-	};
+	PLAINTEXT(false),
+	SKULPT(true),
+	PI(true);
 	
 	DocumentType(final boolean r) {
 		runnable = r;
 	}
 
-	abstract public Editor createEditor(Document d, boolean runOnly);
-	
 	public final boolean runnable;
 	
 }

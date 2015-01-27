35b935e6b9a7e98c3c37907d7458759089ba632d
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-27 14:22:58 +0000

    Javadocs

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/DocumentType.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/DocumentType.java
@@ -1,15 +1,20 @@
 package uk.ac.cam.grpproj.lima.flashmoblearning;
 
+/** A document type */
 public enum DocumentType {
 	
+	/** A plain text document */
 	PLAINTEXT(false),
+	/** A Skulpt program */
 	SKULPT(true),
+	/** A Sonic Pi program */
 	PI(true);
 	
 	DocumentType(final boolean r) {
 		runnable = r;
 	}
 
+	/** Is this a program that can be run, previewed etc? */
 	public final boolean runnable;
 	
 }

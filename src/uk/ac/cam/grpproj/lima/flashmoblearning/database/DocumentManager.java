2092e3feb4ff8842b93e5013443524710434c35e
spencerthang <spencerthang@gmail.com>
2015-01-29 23:28:20 +0000

    Major refactoring - PublishedDocument and WIPDocument now have similar structures, except PublishedDocument will only have 1 revision (latest) - all older ones truncated. Combined many WIP/Published document functions. Added the ability to create a tag. Added NoSuchObject and DuplicateName exceptions where appropriate.

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/database/DocumentManager.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/database/DocumentManager.java
@@ -28,72 +28,61 @@ public abstract class DocumentManager {
 	}
 	
 	/** Get the documents a user has published */
-	public abstract List<PublishedDocument> getPublishedByUser(User u, QueryParam param); 
+	public abstract List<PublishedDocument> getPublishedByUser(User u, QueryParam param) throws NoSuchObjectException;
 	
 	/** Get the documents a user is working on */
-	public abstract List<WIPDocument> getWorkInProgressByUser(User u, QueryParam param);
+	public abstract List<WIPDocument> getWorkInProgressByUser(User u, QueryParam param) throws NoSuchObjectException;
 	
 	/** Get documents by tag */
-	public abstract List<PublishedDocument> getPublishedByTag(Tag t, QueryParam param);
+	public abstract List<PublishedDocument> getPublishedByTag(Tag t, QueryParam param) throws NoSuchObjectException;
 
 	/** Get featured documents */
-	public abstract List<PublishedDocument> getFeatured(QueryParam param);
+	public abstract List<PublishedDocument> getFeatured(QueryParam param) throws NoSuchObjectException;
 	
 	/** Get all featured documents by tag */
-	public abstract List<PublishedDocument> getFeaturedByTag(Tag t, QueryParam param);
+	public abstract List<PublishedDocument> getFeaturedByTag(Tag t, QueryParam param) throws NoSuchObjectException;
 	
 	/** Get all documents published. Should then be sorted by the ResultList. */
-	public abstract List<PublishedDocument> getPublished(QueryParam param);
+	public abstract List<PublishedDocument> getPublished(QueryParam param) throws NoSuchObjectException;
 	
 	/** Delete all documents owned by a user. Called by Login.deleteUser(). */
-	public abstract void deleteUser(User u, QueryParam param);
+	public abstract void deleteAllDocumentsByUser(User u, QueryParam param) throws NoSuchObjectException;
 	
-	/** Get all revisions of a given Work In Progress document */
-	public abstract LinkedList<Revision> getRevisions(WIPDocument d, QueryParam param);
-	
-	/** Get the one and only revision of a given PublishedDocument */
-	public abstract Revision getFinalRevision(PublishedDocument d, QueryParam param);
-	
-	// Note that WIPDocument's and PublishedDocument's are stored differently due to issues
-	// with Revision's: WIPDocument's have many revisions in a separate table, PublishedDocument's
-	// have one which is stored with the rest of the record. Revision's can be large.
+	/** Get all revisions of a given document; published documents will have a single revision */
+	public abstract LinkedList<Revision> getRevisions(Document d, QueryParam param) throws NoSuchObjectException;
 	
 	/** Delete a work-in-progress document. */
-	public abstract void deleteDocument(WIPDocument d);
+	public abstract void deleteDocument(Document d) throws NoSuchObjectException;
 	
-	/** Delete a published document. Be careful with forks! */
-	public abstract void deleteDocument(PublishedDocument d);
+	/** Called when a revision is added to a document */
+	public abstract void addRevision(Document d, Revision r) throws NoSuchObjectException;
 	
-	/** Called when a revision is added to a Work In Progress document */
-	public abstract void addRevision(WIPDocument d, Revision r);
+	/** Add a new document, either with no revisions or with a single revision based on another Document. */
+	public abstract void createDocument(Document d) throws NoSuchObjectException;
 	
-	/** Add a new Work In Progress document, either with no revisions or with a single
-	 * revision based on another Document. */
-	public abstract void createDocument(WIPDocument d);
-	
-	/** Add a new published document. The revision is stored with the document. */
-	public abstract void createDocument(PublishedDocument d);
-	
-	/** Update a document's metadata (not revisions).
-	 * Must implement both subclasses. */
-	public abstract void updateDocument(Document d);
+	/** Update a document's metadata (not revisions). **/
+	/** If a document's status changes from unpublished to published, all revisions but the latest is truncated **/
+	public abstract void updateDocument(Document d) throws NoSuchObjectException;
 	
 	/** List all tags. Note tags are deleted once all references are deleted. */
-	public abstract Set<Tag> getTags();
+	public abstract Set<Tag> getTags() throws NoSuchObjectException;
 	
 	/** Get tag by name */
-	public abstract Tag getTag(String name);
+	public abstract Tag getTag(String name) throws NoSuchObjectException;
+
+	/** Create tag **/
+	public abstract void createTag(Tag tag) throws NoSuchObjectException, DuplicateNameException;
 	
 	/** Update a document's tags (stored separately) */
-	public abstract void updateTags(Document d);
+	public abstract void updateTags(Document d) throws NoSuchObjectException;
 	
 	/** Update a tag. I.e. it may go from banned to unbanned o vice versa. */
-	public abstract void updateTag(Tag tag);
+	public abstract void updateTag(Tag tag) throws NoSuchObjectException, DuplicateNameException;
 	
 	/** Delete a tag. Database will delete references from all documents to this tag. */
-	public abstract void deleteTag(Tag tag);
+	public abstract void deleteTag(Tag tag) throws NoSuchObjectException;
 
 	/** Add a (positive) vote on a given document */
-	public abstract void addVote(User u, PublishedDocument d);
+	public abstract void addVote(User u, PublishedDocument d) throws NoSuchObjectException;
 
 }

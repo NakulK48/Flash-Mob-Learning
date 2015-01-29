6b4ea5ed3aefe19b6b733e0047f0880bf88ad580
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-29 21:35:16 +0000

    Revisions are fetched from database and *CACHED* by the classes.
    Important for getting the database API right.

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/PublishedDocument.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/PublishedDocument.java
@@ -1,13 +1,16 @@
 package uk.ac.cam.grpproj.lima.flashmoblearning;
 
+import java.lang.ref.SoftReference;
+
 import uk.ac.cam.grpproj.lima.flashmoblearning.database.DocumentManager;
+import uk.ac.cam.grpproj.lima.flashmoblearning.database.QueryParam;
 
 /** A document that has been published for everyone to see. Note that the content of the document
  * cannot be modified once it has been published, but the tags and title can, by the owner. */
 public class PublishedDocument extends Document{
 	
 	/** Content stored as a Revision */
-	private final Revision content;
+	private SoftReference<Revision> contentRef;
 	
 	/** Has the "featured" flag been set by the administrator? */
 	private boolean isFeatured;
@@ -15,12 +18,29 @@ public class PublishedDocument extends Document{
 	public PublishedDocument(WIPDocument original) {
 		super(original.docType, original.owner, original.parentDoc, original.getTitle());
 		isFeatured = false;
-		content = original.getLastRevision();
 	}
 	
 	/** Get the one and only revision */
 	public Revision getContentRevision() {
-		return content;
+		synchronized(this) {
+			if(contentRef != null) {
+				Revision r = contentRef.get();
+				if(r != null) {
+					return r;
+				} else {
+					contentRef = null;
+				}
+			}
+		}
+		Revision r = DocumentManager.getInstance().getFinalRevision(this, QueryParam.UNSORTED);
+		synchronized(this) {
+			if(contentRef != null) {
+				Revision r1 = contentRef.get();
+				if(r1 != null) return r1;
+			}
+			contentRef = new SoftReference<Revision>(r);
+		}
+		return r;
 	}
 
 	/** Copy a document so we can edit it */
--- src/uk/ac/cam/grpproj/lima/flashmoblearning/WIPDocument.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/WIPDocument.java
@@ -1,32 +1,56 @@
 package uk.ac.cam.grpproj.lima.flashmoblearning;
 
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 import uk.ac.cam.grpproj.lima.flashmoblearning.database.DocumentManager;
+import uk.ac.cam.grpproj.lima.flashmoblearning.database.QueryParam;
 
 /** A document that is still being worked on. Has a revision history. Has not been published. */
 public class WIPDocument extends Document {
 	
 	public WIPDocument(PublishedDocument forked, User newOwner) {
 		super(forked.docType, newOwner, forked, forked.getTitle());
-		revisions = new LinkedList<Revision>();
 	}
 	
-	private final LinkedList<Revision> revisions;
+	private SoftReference<LinkedList<Revision>> revisionsRef;
 
+	public List<Revision> getRevisions() {
+		return Collections.unmodifiableList(new ArrayList<Revision>(innerGetRevisions()));
+	}
+	
 	/** Find all revisions */
-	public synchronized List<Revision> getRevisions() {
-		return Collections.unmodifiableList(new ArrayList<Revision>(revisions));
+	private LinkedList<Revision> innerGetRevisions() {
+		synchronized(this) {
+			if(revisionsRef != null) {
+				LinkedList<Revision> r = revisionsRef.get();
+				if(r != null) {
+					return r;
+				} else {
+					revisionsRef = null;
+				}
+			}
+		}
+		LinkedList<Revision> revisions = DocumentManager.getInstance().getRevisions(this, QueryParam.UNSORTED);
+		synchronized(this) {
+			if(revisionsRef != null) {
+				LinkedList<Revision> r1 = revisionsRef.get();
+				if(r1 != null) return r1;
+			}
+			revisionsRef = new SoftReference<LinkedList<Revision>>(revisions);
+		}
+		return revisions;
 	}
 	
 	/** Called when a new revision is saved */
 	public void saveRevision(Revision r) {
 		synchronized(this) {
-			assert(!revisions.contains(r));
-			revisions.add(r);
+			if(revisionsRef != null && revisionsRef.get() != null) {
+				revisionsRef.get().add(r);
+			}
 		}
 		DocumentManager.getInstance().addRevision(this, r);
 	}
@@ -38,9 +62,8 @@ public class WIPDocument extends Document {
 	}
 
 	public Revision getLastRevision() {
-		synchronized(this) {
-			return revisions.getLast();
-		}
+		// FIXME OPT Consider caching separately. Be careful with consistency between the two!
+		return innerGetRevisions().getLast();
 	}
 
 }

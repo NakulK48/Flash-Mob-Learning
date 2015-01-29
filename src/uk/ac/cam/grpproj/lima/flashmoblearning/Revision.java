808574502ed0517538e06cf3f71882ae409b032b
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-29 20:47:06 +0000

    Flesh out Revision and document a potentially serious issue with storing pages

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/Revision.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/Revision.java
@@ -4,15 +4,28 @@ import java.util.Date;
 
 /** Represents a single version of a document. Note that this does not necessarily correspond to a
  * single database table. */
-public abstract class Revision {
+public class Revision {
 	
 	/** Owned by a specific user */
-	public abstract User getOwner();
+	public final User owner;
+	/** Creation time */
+	public final Date creationTime;
+	/** Content. May be relatively large, but no more than a few pages. In any case Revision's
+	 * should be short-lived. */
+	private final String content;
+	// FIXME OPT Consider:
+	// 1) Lazily fetching revisions. E.g. keep a RevisionTag with a weak link to a Revision.
+	// 2) Lazily fetching content. E.g. keep a weak link to the content.
+	// THIS AFFECTS DATABASE SCHEMA! But complexity, best to look at it after have working system.
 	
-	/** Created at... */
-	public abstract Date getCreationTime();
+	public Revision(User u, Date d, String c) {
+		owner = u;
+		creationTime = d;
+		content = c;
+	}
 	
-	/** Content */
-	public abstract String getContent();
+	public String getContent() {
+		return content;
+	}
 
 }

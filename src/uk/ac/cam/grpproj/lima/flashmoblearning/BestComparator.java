ca5166c3aea5ea685ce14e89da3a899509b19059
Nakul Khanna <nakul.khanna@gmail.com>
2015-01-31 15:20:23 +0000

    removed publish time
    Realised it corresponds to creation time

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/BestComparator.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/BestComparator.java
@@ -7,12 +7,14 @@ public class BestComparator implements Comparator<PublishedDocument>
 
 	@Override
 	public int compare(PublishedDocument o1, PublishedDocument o2) {
-		long t1 = o1.getPublishTime();
-		long t2 = o2.getPublishTime();
+		long t1 = o1.creationTime;
+		long t2 = o2.creationTime;
+		
+		long a1 = System.currentTimeMillis() - t1;
+		long a2 = System.currentTimeMillis() - t2;
 		
 		int s1 = o1.getScore();
 		int s2 = o2.getScore();
-		
 		
 		
 		return 0;

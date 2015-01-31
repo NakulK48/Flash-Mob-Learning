f6fde12ba98f7b8c0af3e2afac107ac51ec83cc4
Nakul Khanna <nakul.khanna@gmail.com>
2015-01-31 15:27:04 +0000

    finished best sorting algorithm

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/BestComparator.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/BestComparator.java
@@ -16,13 +16,15 @@ public class BestComparator implements Comparator<PublishedDocument>
 		int s1 = o1.getScore();
 		int s2 = o2.getScore();
 		
+		int r1 = calculateRanking(a1, s1);
+		int r2 = calculateRanking(a2, s2);
 		
-		return 0;
+		return (r1-r2);
 	}
 	
 	public static int calculateRanking(long age, int score)
 	{
-		
+		return (int) (age * Math.exp(-8 * age * age));
 	}
 
 }

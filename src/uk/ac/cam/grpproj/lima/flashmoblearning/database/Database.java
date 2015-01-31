031f4e3c21ff6ddc950fc68e874d3ad86ccbe35a
spencerthang <spencerthang@gmail.com>
2015-01-31 18:45:31 +0000

    Multi-statement queries not supported, split creation into 6 queries.

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/database/Database.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/database/Database.java
@@ -17,8 +17,8 @@ public class Database {
 	private LoginManager m_LoginManagerInstance;
 	private DocumentManager m_DocumentManagerInstance;
 
-	private static final String c_Username = "flashmoblearning";
-	private static final String c_Password = "flashmoblearning";
+	private static final String c_Username = "root";
+	private static final String c_Password = "";
 	private static final String c_JDBCURL = "jdbc:mysql://localhost/flashmoblearning";
 	
 	public static Database getInstance() {
@@ -28,7 +28,8 @@ public class Database {
 	/** Initializes and tests the database connection, setting it up if necessary **/
 	public static void init() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection connection = DriverManager.getConnection(c_JDBCURL + "?user=" + c_Username + "?password" + c_Password);
+		System.out.println(c_JDBCURL + "?user=" + c_Username + "&password" + c_Password);
+		Connection connection = DriverManager.getConnection(c_JDBCURL + "?user=" + c_Username + "&password=" + c_Password);
 		setup(connection);
 		m_Instance = new Database(connection);
 	}
@@ -38,12 +39,12 @@ public class Database {
 	}
 
 	/** Obtain database connection **/
-	protected Connection getConnection() throws SQLException {
+	public Connection getConnection() throws SQLException {
 		return m_Connection;
 	}
 
 	/** Obtain a new statement, ready for execution **/
-	protected Statement getStatement() throws SQLException {
+	public Statement getStatement() throws SQLException {
 		return m_Connection.createStatement();
 	}
 
@@ -74,11 +75,7 @@ public class Database {
 
 	/** Creates all necessary tables if they do not exist. **/
 	private static void setup(Connection connection) throws SQLException {
-		String create_sql = "--\n" +
-				"-- Table structure for table `documents`\n" +
-				"--\n" +
-				"\n" +
-				"CREATE TABLE IF NOT EXISTS `documents` (\n" +
+		String create_documents = "CREATE TABLE IF NOT EXISTS `documents` (\n" +
 				"  `id` int(11) NOT NULL,\n" +
 				"  `user_id` int(11) NOT NULL,\n" +
 				"  `type` int(11) NOT NULL,\n" +
@@ -88,78 +85,52 @@ public class Database {
 				"  `update_time` timestamp NOT NULL,\n" +
 				"  `vote_count` int(11) NOT NULL DEFAULT '0',\n" +
 				"  PRIMARY KEY (`id`)\n" +
-				") ENGINE=InnoDB DEFAULT CHARSET=latin1;\n" +
-				"\n" +
-				"-- --------------------------------------------------------\n" +
-				"\n" +
-				"--\n" +
-				"-- Table structure for table `document_tags`\n" +
-				"--\n" +
-				"\n" +
-				"CREATE TABLE IF NOT EXISTS `document_tags` (\n" +
+				") ENGINE=InnoDB DEFAULT CHARSET=latin1;\n";
+
+		String create_document_tags = "CREATE TABLE IF NOT EXISTS `document_tags` (\n" +
 				"  `id` int(11) NOT NULL,\n" +
 				"  `tag_id` int(11) NOT NULL,\n" +
 				"  `document_id` int(11) NOT NULL,\n" +
 				"  PRIMARY KEY (`id`)\n" +
-				") ENGINE=InnoDB DEFAULT CHARSET=latin1;\n" +
-				"\n" +
-				"-- --------------------------------------------------------\n" +
-				"\n" +
-				"--\n" +
-				"-- Table structure for table `revisions`\n" +
-				"--\n" +
-				"\n" +
-				"CREATE TABLE IF NOT EXISTS `revisions` (\n" +
+				") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+
+		String create_revisions = "CREATE TABLE IF NOT EXISTS `revisions` (\n" +
 				"  `id` int(11) NOT NULL,\n" +
 				"  `document_id` int(11) NOT NULL,\n" +
 				"  `creation_time` timestamp NOT NULL,\n" +
 				"  `content` text NOT NULL,\n" +
 				"  PRIMARY KEY (`id`)\n" +
-				") ENGINE=InnoDB DEFAULT CHARSET=latin1;\n" +
-				"\n" +
-				"-- --------------------------------------------------------\n" +
-				"\n" +
-				"--\n" +
-				"-- Table structure for table `tags`\n" +
-				"--\n" +
-				"\n" +
-				"CREATE TABLE IF NOT EXISTS `tags` (\n" +
+				") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+
+		String create_tags = "CREATE TABLE IF NOT EXISTS `tags` (\n" +
 				"  `id` int(11) NOT NULL,\n" +
 				"  `name` text NOT NULL,\n" +
 				"  `banned_flag` tinyint(1) NOT NULL DEFAULT '0',\n" +
 				"  `reference_count` int(11) NOT NULL DEFAULT '0',\n" +
 				"  PRIMARY KEY (`id`)\n" +
-				") ENGINE=InnoDB DEFAULT CHARSET=latin1;\n" +
-				"\n" +
-				"-- --------------------------------------------------------\n" +
-				"\n" +
-				"--\n" +
-				"-- Table structure for table `users`\n" +
-				"--\n" +
-				"\n" +
-				"CREATE TABLE IF NOT EXISTS `users` (\n" +
+				") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+
+		String create_users	 = "CREATE TABLE IF NOT EXISTS `users` (\n" +
 				"  `id` int(11) NOT NULL,\n" +
 				"  `username` varchar(255) NOT NULL,\n" +
 				"  `password` varchar(255) NOT NULL,\n" +
 				"  `teacher_flag` tinyint(1) NOT NULL DEFAULT '0',\n" +
 				"  PRIMARY KEY (`id`)\n" +
-				") ENGINE=InnoDB DEFAULT CHARSET=latin1;\n" +
-				"\n" +
-				"-- --------------------------------------------------------\n" +
-				"\n" +
-				"--\n" +
-				"-- Table structure for table `votes`\n" +
-				"--\n" +
-				"\n" +
-				"CREATE TABLE IF NOT EXISTS `votes` (\n" +
+				") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+
+		String create_votes = "CREATE TABLE IF NOT EXISTS `votes` (\n" +
 				"  `user_id` int(11) NOT NULL,\n" +
 				"  `document_id` int(11) NOT NULL,\n" +
 				"  PRIMARY KEY (`user_id`,`document_id`)\n" +
 				") ENGINE=InnoDB DEFAULT CHARSET=latin1;\n";
 
-
 		Statement statement = connection.createStatement();
-		statement.execute(create_sql);
+		statement.execute(create_documents);
+		statement.execute(create_document_tags);
+		statement.execute(create_revisions);
+		statement.execute(create_tags);
+		statement.execute(create_users);
+		statement.execute(create_votes);
 	}
 	
 }

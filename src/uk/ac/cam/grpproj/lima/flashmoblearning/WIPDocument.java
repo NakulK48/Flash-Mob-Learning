5560ccf7369c96d5986d41ea41cc3ef0ba2bdea7
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-31 14:22:57 +0000

    Add constructor just for DocumentManager

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/WIPDocument.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/WIPDocument.java
@@ -19,6 +19,12 @@ public class WIPDocument extends Document {
 	WIPDocument(PublishedDocument forked, User newOwner, Revision initialRevision) throws NotInitializedException, SQLException, NoSuchObjectException {
 		super(forked.docType, newOwner, forked, forked.getTitle(), System.currentTimeMillis());
 	}
+
+	/** Only called by DocumentManager */
+	public WIPDocument(DocumentType docType, User owner, Document parentDoc,
+			String title, long time) {
+		super(docType, owner, parentDoc, title, time);
+	}
 	
 	/** Publish as a PublishedDocument. Creates a new PublishedDocument using the final revision
 	 * and calls the database to store it. */

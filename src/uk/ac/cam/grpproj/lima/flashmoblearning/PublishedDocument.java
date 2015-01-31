9714c9ddd47aad2f01ba1e347196e94a9ee41c1b
Matthew Toseland <matthew@toselandcs.co.uk>
2015-01-31 14:17:33 +0000

    Fix issues with IDs and Document's/User's.
    I still think we should preallocate IDs but whatever, it's easy enough to do it this way.

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/PublishedDocument.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/PublishedDocument.java
@@ -1,16 +1,14 @@
 package uk.ac.cam.grpproj.lima.flashmoblearning;
 
-import java.lang.ref.SoftReference;
+import java.sql.SQLException;
 
 import uk.ac.cam.grpproj.lima.flashmoblearning.database.DocumentManager;
-import uk.ac.cam.grpproj.lima.flashmoblearning.database.QueryParam;
+import uk.ac.cam.grpproj.lima.flashmoblearning.database.exception.NoSuchObjectException;
+import uk.ac.cam.grpproj.lima.flashmoblearning.database.exception.NotInitializedException;
 
 /** A document that has been published for everyone to see. Note that the content of the document
  * cannot be modified once it has been published, but the tags and title can, by the owner. */
 public class PublishedDocument extends Document{
-	
-	/** Content stored as a Revision */
-	private SoftReference<Revision> contentRef;
 	
 	/** Has the "featured" flag been set by the administrator? */
 	private boolean isFeatured;
@@ -20,31 +18,19 @@ public class PublishedDocument extends Document{
 		isFeatured = false;
 	}
 	
-	/** Get the one and only revision */
-	public Revision getContentRevision() {
-		synchronized(this) {
-			if(contentRef != null) {
-				Revision r = contentRef.get();
-				if(r != null) {
-					return r;
-				} else {
-					contentRef = null;
-				}
-			}
-		}
-		Revision r = DocumentManager.getInstance().getFinalRevision(this, QueryParam.UNSORTED);
-		synchronized(this) {
-			if(contentRef != null) {
-				Revision r1 = contentRef.get();
-				if(r1 != null) return r1;
-			}
-			contentRef = new SoftReference<Revision>(r);
-		}
-		return r;
+	/** Get the one and only revision 
+	 * @throws NoSuchObjectException 
+	 * @throws SQLException 
+	 * @throws NotInitializedException */
+	public Revision getContentRevision() throws NotInitializedException, SQLException, NoSuchObjectException {
+		return getLastRevision();
 	}
 
-	/** Copy a document so we can edit it */
-	public WIPDocument fork(User newOwner) {
+	/** Copy a document so we can edit it 
+	 * @throws NoSuchObjectException 
+	 * @throws SQLException 
+	 * @throws NotInitializedException */
+	public WIPDocument fork(User newOwner) throws NotInitializedException, SQLException, NoSuchObjectException {
 		return new WIPDocument(this, newOwner, getContentRevision());
 	}
 	
@@ -53,8 +39,11 @@ public class PublishedDocument extends Document{
 		return isFeatured;
 	}
 	
-	/** Set the Featured flag */
-	public void setFeatured(boolean set) {
+	/** Set the Featured flag 
+	 * @throws NoSuchObjectException 
+	 * @throws SQLException 
+	 * @throws NotInitializedException */
+	public void setFeatured(boolean set) throws NotInitializedException, SQLException, NoSuchObjectException {
 		synchronized(this) {
 			if(set == isFeatured) return;
 			isFeatured = set;
--- src/uk/ac/cam/grpproj/lima/flashmoblearning/WIPDocument.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/WIPDocument.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.grpproj.lima.flashmoblearning;
 
 import java.lang.ref.SoftReference;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -8,63 +9,21 @@ import java.util.List;
 
 import uk.ac.cam.grpproj.lima.flashmoblearning.database.DocumentManager;
 import uk.ac.cam.grpproj.lima.flashmoblearning.database.QueryParam;
+import uk.ac.cam.grpproj.lima.flashmoblearning.database.exception.NoSuchObjectException;
+import uk.ac.cam.grpproj.lima.flashmoblearning.database.exception.NotInitializedException;
 
 /** A document that is still being worked on. Has a revision history. Has not been published. */
 public class WIPDocument extends Document {
 	
-	public WIPDocument(PublishedDocument forked, User newOwner, Revision initialRevision) {
+	public WIPDocument(PublishedDocument forked, User newOwner, Revision initialRevision) throws NotInitializedException, SQLException, NoSuchObjectException {
 		super(forked.docType, newOwner, forked, forked.getTitle(), System.currentTimeMillis());
 		DocumentManager.getInstance().addRevision(this, initialRevision);
-	}
-	
-	private SoftReference<LinkedList<Revision>> revisionsRef;
-
-	public List<Revision> getRevisions() {
-		return Collections.unmodifiableList(new ArrayList<Revision>(innerGetRevisions()));
-	}
-	
-	/** Find all revisions */
-	private LinkedList<Revision> innerGetRevisions() {
-		synchronized(this) {
-			if(revisionsRef != null) {
-				LinkedList<Revision> r = revisionsRef.get();
-				if(r != null) {
-					return r;
-				} else {
-					revisionsRef = null;
-				}
-			}
-		}
-		LinkedList<Revision> revisions = DocumentManager.getInstance().getRevisions(this, QueryParam.UNSORTED);
-		synchronized(this) {
-			if(revisionsRef != null) {
-				LinkedList<Revision> r1 = revisionsRef.get();
-				if(r1 != null) return r1;
-			}
-			revisionsRef = new SoftReference<LinkedList<Revision>>(revisions);
-		}
-		return revisions;
-	}
-	
-	/** Called when a new revision is saved */
-	public void saveRevision(Revision r) {
-		synchronized(this) {
-			if(revisionsRef != null && revisionsRef.get() != null) {
-				revisionsRef.get().add(r);
-			}
-		}
-		DocumentManager.getInstance().addRevision(this, r);
 	}
 	
 	/** Publish as a PublishedDocument. Creates a new PublishedDocument using the final revision
 	 * and calls the database to store it. */
 	public PublishedDocument publish() {
 		return new PublishedDocument(this);
-	}
-
-	public Revision getLastRevision() {
-		// FIXME OPT Consider caching separately. Be careful with consistency between the two!
-		return innerGetRevisions().getLast();
 	}
 
 }

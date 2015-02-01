e5fd0ea87d6154d159d19eb6fa02b91efcebe4fa
spencerthang <spencerthang@gmail.com>
2015-02-01 04:06:05 +0000

    Expanded DocumentType to support easy conversion to/from integer.

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/DocumentType.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/DocumentType.java
@@ -4,17 +4,34 @@ package uk.ac.cam.grpproj.lima.flashmoblearning;
 public enum DocumentType {
 	
 	/** A plain text document */
-	PLAINTEXT(false),
+	PLAINTEXT(false, 0),
 	/** A Skulpt program */
-	SKULPT(true),
+	SKULPT(true, 1),
 	/** A Sonic Pi program */
-	PI(true);
+	PI(true, 2);
 	
-	DocumentType(final boolean r) {
+	DocumentType(final boolean r, int i) {
 		runnable = r;
+		id = i;
 	}
 
 	/** Is this a program that can be run, previewed etc? */
 	public final boolean runnable;
-	
+	public final int id;
+
+	public int getId() { return id; }
+
+	private static DocumentType[] values = null;
+	public static DocumentType getValue(int id) {
+		if(DocumentType.values == null) {
+			DocumentType.values = DocumentType.values();
+		}
+		for(int i = 0; i < values.length; i++)
+		{
+			if(values[i].getId() == id)
+				return values[i];
+		}
+		return PLAINTEXT;
+	}
+
 }

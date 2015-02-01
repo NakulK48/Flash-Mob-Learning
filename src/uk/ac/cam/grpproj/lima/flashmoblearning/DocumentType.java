6ced7614b18798dda4ecbe8f4ff790c238b0cf7d
Matthew Toseland <matthew@toselandcs.co.uk>
2015-02-01 20:33:55 +0000

    Document odd enum id

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/DocumentType.java
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/DocumentType.java
@@ -10,6 +10,11 @@ public enum DocumentType {
 	/** A Sonic Pi program */
 	PI(true, 2);
 	
+	/** @param r Whether this is a program that can be run.
+	 * @param i Permanent integer ID. Not the same as the ordinal()!; we can 
+	 * add more document types and remove old ones and everything will work, as
+	 * long as we never change an ID for a document type we are using.
+	 */
 	DocumentType(final boolean r, int i) {
 		runnable = r;
 		id = i;
@@ -17,6 +22,9 @@ public enum DocumentType {
 
 	/** Is this a program that can be run, previewed etc? */
 	public final boolean runnable;
+	/** Integer ID for the document type. Will not be changed even if we add 
+	 * more document types or remove old document types. Different to 
+	 * ordinal()! */
 	public final int id;
 
 	public int getId() { return id; }

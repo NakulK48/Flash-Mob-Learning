f714b65c8608a4aedb057a2e5e64d677fe9483cc
Matthew Toseland <matthew@toselandcs.co.uk>
2015-02-05 19:52:50 +0000

    Boring tests but ensure uniqueness

---
+++ test/uk/ac/cam/grpproj/lima/flashmoblearning/DocumentTypeTest.java
@@ -0,0 +1,21 @@
+package uk.ac.cam.grpproj.lima.flashmoblearning;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class DocumentTypeTest {
+	
+	@Test
+	public void testValid() {
+		for(DocumentType t : DocumentType.values()) {
+			Assert.assertEquals(t, DocumentType.getValue(t.id));
+		}
+	}
+	
+	@Test
+	public void testInvalid() {
+		Assert.assertEquals(DocumentType.PLAINTEXT, DocumentType.getValue(-1));
+	}
+
+}

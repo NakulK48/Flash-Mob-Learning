b39e2c98fe0bb801f21a8f034c741d195f8bb876
spencerthang <spencerthang@gmail.com>
2015-02-19 15:38:17 +0000

    Removed already initialized exception - made obsolete.

--- src/uk/ac/cam/grpproj/lima/flashmoblearning/database/exception/AlreadyInitializedException.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.cam.grpproj.lima.flashmoblearning.database.exception;
-
-import java.sql.SQLException;
-
-/**
- * Created by Spencer on 12/2/2015.
- */
-public class AlreadyInitializedException extends SQLException {
-}

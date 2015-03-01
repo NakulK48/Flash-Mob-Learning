6d99bae6e3edbf48370de350ea7a120c213eb162
Matthew Toseland <matthew@toselandcs.co.uk>
2015-03-01 18:23:43 +0000

    Add HTMLEncoder, use this to encode strings being included in HTML to prevent XSS

---
+++ src/uk/ac/cam/grpproj/lima/flashmoblearning/HTMLEncoder.java
@@ -0,0 +1,30 @@
+package uk.ac.cam.grpproj.lima.flashmoblearning;
+
+public class HTMLEncoder {
+	
+	static final String BLACKLIST = "<>&\"'";
+	
+	static final String[] BLACKLIST_ENCODED = {
+		"&lt;",
+		"&gt;",
+		"&amp;",
+		"&quot;",
+		"&#39;"
+	};
+	
+	public static String encode(String s) {
+		assert(BLACKLIST.length() == BLACKLIST_ENCODED.length);
+		StringBuilder sb = new StringBuilder(s.length());
+		for(int i=0;i<s.length();i++) {
+			char c = s.charAt(i);
+			int idx = BLACKLIST.indexOf(c);
+			if(idx >= 0) {
+				sb.append(BLACKLIST_ENCODED[idx]);
+			} else {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
+
+}
---
+++ test/uk/ac/cam/grpproj/lima/flashmoblearning/HTMLEncoderTest.java
@@ -0,0 +1,32 @@
+package uk.ac.cam.grpproj.lima.flashmoblearning;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HTMLEncoderTest {
+	
+	String in1 = "<test>";
+	String out1 = "&lt;test&gt;";
+	String in2 = "";
+	String out2 = "";
+	String in3 = "\"";
+	String out3 = "&quot;";
+	String in4 = "'";
+	String out4 = "&#39;";
+	String in5 = "hello";
+	String out5 = in5;
+	
+	@Test
+	public void testEncode() {
+		test(in1, out1);
+		test(in2, out2);
+		test(in3, out3);
+		test(in4, out4);
+		test(in5, out5);
+	}
+
+	private void test(String in, String out) {
+		Assert.assertEquals(out, HTMLEncoder.encode(in));
+	}
+
+}
